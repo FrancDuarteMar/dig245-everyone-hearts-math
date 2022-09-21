0c67c62d6bd439e6a8f535f85a91655d198749b4
Francisco Duarte M <frduartemartinez@davidson.edu>
2022-09-21 15:41:32 -0400

    Added in class demo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,28 @@
 
 /* javascript */
+// $("h1").hide();
+
+$(".click-me").click(function(e) { 
+    console.log("Clicked the button",e);
+    
+});
+
+let username = "";
+let requiredUsername = "secret"
+
+$("form").submit(function (e) { 
+    e.preventDefault();
+    username = $(".username").val();   
+    console.log(username);
+
+    let str = ""
+    if(username == requiredUsername){
+        str=`<div class="alert alert-success" role="alert">
+        Welcome!</div>`;
+    }
+    else{
+        str =`<div class="alert alert-danger" role="alert"> Danger! </div>`;
+    }
+    
+    $(".output").html(str);
+});
