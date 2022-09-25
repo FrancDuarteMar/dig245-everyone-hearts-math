661b602137fe33c36e7022c4c097cda130bd9dde
Francisco Duarte M <frduartemartinez@davidson.edu>
2022-09-25 17:56:44 -0400

    Updated the text and distances

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,50 +32,50 @@ $('#solutions').on('click', function(e){
       "width":"", "background-color":"","position":""});
     }
     else if(selectionIndex == 1){
-      $(".optimal-solution-description").html("This is the shortest path!")
+      $(".optimal-solution-description").html("This is the shortest path guaranteed to find your friend")
       $(".selected-solution-description").html("But... you might not find your friend")
       
       $(".selected-solution").css({"height":maxWidth*1/8,
-        "width":"5px", "background-color":"white","position":"relative"});
+        "width":"5px", "background-color":"green","position":"relative"});
       $("#optimal-solution").css({"height": maxWidth,
-        "width":"5px", "background-color":"white","position":"relative"});
+        "width":"5px", "background-color":"red","position":"relative"});
 
     }
     else if(selectionIndex == 2){
       $(".optimal-solution-description").html("This is the shortest path!")
-      $(".selected-solution-description").html("It takes a while to walk everywhere huh")
+      $(".selected-solution-description").html("Almost but not quite!")
 
       $(".selected-solution").css({"height":maxWidth+(maxWidth*1/8),
-        "width":"5px", "background-color":"white","position":"relative"});
+        "width":"5px", "background-color":"red","position":"relative"});
       $("#optimal-solution").css({"height":maxWidth,
-        "width":"5px", "background-color":"white","position":"relative"});
+        "width":"5px", "background-color":"green","position":"relative"});
     }
     else if(selectionIndex == 3){
       $(".optimal-solution-description").html("This is the shortest path!")
-      $(".selected-solution-description").html("But... you might not find your friend")
+      $(".selected-solution-description").html("Good luck with your walk!")
 
       $(".selected-solution").css({"height":maxWidth**2,
-        "width":"5px", "background-color":"white","position":"relative"});
+        "width":"5px", "background-color":"red","position":"relative"});
       $("#optimal-solution").css({"height": maxWidth,
-        "width":"5px", "background-color":"white","position":"relative"});
+        "width":"5px", "background-color":"green","position":"relative"});
     }
     else if(selectionIndex == 4){
-      $(".optimal-solution-description").html("This is the shortest path!")
-      $(".selected-solution-description").html("But... you might not find your friend")
+      $(".optimal-solution-description").html("Hey they're the same distance!")
+      $(".selected-solution-description").html("That's it!")
 
-      $(".selected-solution").css({"height":2*maxWidth,
-        "width":"5px", "background-color":"white","position":"relative"});
+      $(".selected-solution").css({"height":maxWidth,
+        "width":"5px", "background-color":"green","position":"relative"});
       $("#optimal-solution").css({"height":maxWidth,
-        "width":"5px", "background-color":"white","position":"relative"});
+        "width":"5px", "background-color":"green","position":"relative"});
     }
     else if(selectionIndex == 5){
       $(".optimal-solution-description").html("This is the shortest path!")
-      $(".selected-solution-description").html("But... you might not find your friend")
+      $(".selected-solution-description").html("Eh, you still had to walk a while")
 
-      $(".selected-solution").css({"height":maxWidth,
-        "width":"5px", "background-color":"white","position":"relative"});
+      $(".selected-solution").css({"height":maxWidth+(maxWidth*7/8),
+        "width":"5px", "background-color":"red","position":"relative"});
       $("#optimal-solution").css({"height":maxWidth,
-        "width":"5px", "background-color":"white","position":"relative"});
+        "width":"5px", "background-color":"green","position":"relative"});
     }
 
 });
